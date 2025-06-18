588cfac579f134b4952b4a4fdb37279d53d662dc
chinmaychahar <chinmay.cc.06@gmail.com>
2025-06-18 05:02:46 +0000

    Deploying to gh-pages from @ chinmaychahar/chinmaychahar.github.io@58e7f17e27f2d305a2e033db1713e77fd689965e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-publications",
-          title: "Publications",
+          title: "publications",
           description: "A list of research work and publications  here.",
           section: "Navigation",
           handler: () => {
