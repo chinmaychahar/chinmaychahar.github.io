cc98d667f643b4eef3a4488331c781b7c2d57f31
chinmaychahar <chinmay.cc.06@gmail.com>
2025-07-09 06:34:41 +0000

    Deploying to gh-pages from @ chinmaychahar/chinmaychahar.github.io@f055be2c7297577b06dff1832fd88cc771fef243 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -407,16 +407,16 @@ ninja.data = [{
           description: "Platform for upcycling donated clothes into adaptive wear.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_rethreads/";
-            },},{id: "projects-noisa",
-          title: 'NOISA',
-          description: "Browser extension for people with vestibular disorders - No Invasive Site Animations.",
+            },},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_noisa/";
-            },},{id: "projects-safeweb",
-          title: 'SafeWeb',
-          description: "Chrome extension promoting the 20-20-20 rule for healthy computer usage.",
+              window.location.href = "/projects/2_project/";
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_safeweb/";
+              window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
