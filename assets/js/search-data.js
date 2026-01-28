02d85c183a96895a6fca7ae419b723695b9f73fa
chinmaychahar <chinmay.cc.06@gmail.com>
2026-01-28 12:22:49 +0000

    Deploying to gh-pages from @ chinmaychahar/chinmaychahar.github.io@115f502423b77e6f1172f9d3d374a64775174337 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-open-source",
-          title: "Open Source",
+          title: "open Source",
           description: "Selected projects, stack, and my recent GitHub activity.",
           section: "Navigation",
           handler: () => {
